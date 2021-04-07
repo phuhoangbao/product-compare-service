cc7b7a3abe3fb0394116f8e26236858054562fc4
phuhoangbao <81680585+phuhoangbao@users.noreply.github.com>
2021-04-07 20:13:24 +0700

    Add files via upload

--- nba-rest-services/nba-fetching-product-service/src/main/java/com/nba/controllers/ProductController.java
+++ nba-rest-services/nba-fetching-product-service/src/main/java/com/nba/controllers/ProductController.java
@@ -40,7 +40,7 @@ public class ProductController {
   private final Bucket bucket;
 
   public ProductController(){
-    Bandwidth limit = Bandwidth.classic(5, Refill.greedy(5, Duration.ofMinutes(1)));
+    Bandwidth limit = Bandwidth.classic(30, Refill.greedy(30, Duration.ofMinutes(1)));
     this.bucket = Bucket4j.builder()
             .addLimit(limit)
             .build();
